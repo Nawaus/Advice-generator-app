7e04e06f6be1a583ad1782533ba1be37eb982aab
Gustavo edson <ggugu9289@gmail.com>
2022-11-12 13:15:23 -0300

    first

---
+++ assets/js/main.js
@@ -0,0 +1,24 @@
+const init = () => {
+  const diceButton = document.querySelector(".dice-button button");
+
+  diceButton.addEventListener("click", () => {
+    try {
+      (async () => {
+        const result = await fetch(`https://api.adviceslip.com/advice`).then(
+          (res) => res.json()
+        );
+        const contentText = document.querySelector(".main-text p");
+        const idAdviceContent = document.querySelector("span");
+        const adviceText = result.slip.advice;
+        const idText = result.slip.id;
+        contentText.textContent = `"${adviceText}"`;
+        idAdviceContent.textContent = idText;
+        console.log(result);
+      })();
+    } catch (error) {
+      console.log(error);
+      console.log("Sorry, something went wrong with the request");
+    }
+  });
+};
+init();
