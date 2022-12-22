c33e7c749d33a12d3fea9a3d5f9d68910434964a
Giovanni Armane <giovanniarmane@gmail.com>
2022-12-22 15:33:43 -0300

    fix(assets/js/main.js): fix firefox cache issues and make first advice random

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,23 +1,30 @@
 const init = () => {
-  const diceButton = document.querySelector(".dice-button button");
+  const getAdvice = async () => (
+    await fetch(`https://api.adviceslip.com/advice`, { cache: 'reload' })
+      .then((res) => res.json())
+    );
 
+  const updateAdvice = async () => {
+    const result = await getAdvice();
+    updateAdviceText(result.slip.id, result.slip.advice);
+  }
+
+  const updateAdviceText = (id, advice) => {
+    document.querySelector("span").textContent = id;
+    document.querySelector(".main-text p").textContent = `"${advice}"`;
+  }
+
+  const diceButton = document.querySelector(".dice-button button");
   diceButton.addEventListener("click", () => {
     try {
-      (async () => {
-        const result = await fetch(`https://api.adviceslip.com/advice`).then(
-          (res) => res.json()
-        );
-        const mainText = document.querySelector(".main-text p");
-        const idAdviceContent = document.querySelector("span");
-        const adviceText = result.slip.advice;
-        const idText = result.slip.id;
-        mainText.textContent = `"${adviceText}"`;
-        idAdviceContent.textContent = idText;
-      })();
+      (async () => updateAdvice())();
     } catch (error) {
       console.log(error);
       console.log("Sorry, something went wrong with the request");
     }
   });
+
+  // Update advice at first draw.
+  updateAdvice();
 };
 init();
